5a56b57e3c9314b0e9e028d8ca5a4a1ac957d625
freeman <iviewn@gmail.com>
2022-11-08 21:02:03 +0300

    start

--- js/custom.js
+++ js/custom.js
@@ -1,10 +1,16 @@
 $(document).ready(function() {
 
     /*top menu toggle*/
-    $('.menu-btn').click(function(){
-        $('.header-nav').toggleClass('vis');
+    
+    $('.hambu').click(function(){
+        $('.header-navgroup').toggleClass('vis');
         $('body').toggleClass('oh');
-        $(this).toggleClass('close');
+        $('#nav-icon4').toggleClass('open');
+    });
+    $('.menu').click(function () {
+        $('.navleft').removeClass('vis');
+        $('body').removeClass('oh');
+        $('#nav-icon4').removeClass('open');
     });
 
     /*end top menu toggle*/
